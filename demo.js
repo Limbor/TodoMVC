afbac8c20e46c39c5e037bec8bf6aae61b30e067
感觉好想睡觉 <18721912858@126.com>
2020-06-19 09:41:11 +0800

    del and favorate

--- demo.js
+++ demo.js
@@ -13,7 +13,12 @@ function updateAmount(itemCount) {
     $('itemCount').innerText = itemCount + " items left";
 }
 
+function flushStorage() {
+    localStorage.setItem('data', JSON.stringify(window.model.data));
+}
+
 function updateItems() {
+    flushStorage();
     let items = window.model.data.items;
     let list = $('itemList');
     list.innerText = '';
@@ -22,23 +27,48 @@ function updateItems() {
         let item = document.createElement("div");
         item.classList.add("item");
         list.appendChild(item);
+
         let checkbox = document.createElement("input");
         checkbox.type = 'checkbox';
         checkbox.id = 'item' + i;
         item.appendChild(checkbox);
+
         let label = document.createElement("label");
         label.htmlFor = checkbox.id;
         item.appendChild(label);
+
         let content = document.createElement("div");
         content.classList.add(CL_CONTENT);
         content.innerText = items[i].msg;
         item.appendChild(content);
+
         let favorite = document.createElement("img");
-        favorite.src = "img/star.png";
+        if(!items[i].favorites) favorite.src = "img/star.png";
+        else favorite.src = "img/star-fill.png";
         item.appendChild(favorite)
-        favorite.addEventListener("click", () => {
-            favorite.src = "img/star-fill.png";
+        favorite.addEventListener("touchend", () => {
+            if(items[i].completed) return;
+            items[i].favorites = !items[i].favorites;
+            if(i !== 0 && items[i].favorites) items.unshift(items.splice(i, 1)[0]);
+            else if(!items[i].favorites){
+                for(let j = i + 1; j < items.length; j++){
+                    if(items[j].favorites) {
+                        items[j] = items.splice(j - 1, 1, items[j])[0];
+                    }
+                    else break;
+                }
+            }
+            updateItems();
         })
+
+        let trash = document.createElement("img");
+        trash.src = 'img/trash.png';
+        item.appendChild(trash);
+        trash.addEventListener('touchend', () => {
+           items.splice(i, 1);
+           updateItems();
+        });
+
         if(!items[i].completed) count++;
         else {
             checkbox.checked = true;
@@ -46,6 +76,15 @@ function updateItems() {
         }
         checkbox.addEventListener("change", () => {
             items[i].completed = checkbox.checked;
+            if(items[i].completed) items.push(items.splice(i, 1)[0]);
+            else{
+                for(let j = i - 1; j >= 0; j--){
+                    if(items[j].completed){
+                        items[j] = items.splice(j + 1, 1, items[j])[0];
+                    }
+                    else break;
+                }
+            }
             updateItems();
         });
     }
@@ -59,12 +98,20 @@ function addItem() {
         alert("输入内容不能为空");
         return;
     }
-    window.model.data.items.unshift({
-       msg: message,
-       completed: false,
-       time: 0,
-       favorites: false
-    });
+    let items = window.model.data.items;
+    let i;
+    for(i = 0; i < items.length; i++){
+        if(!items[i].favorites) break;
+    }
+    let item = {
+        msg: message,
+        completed: false,
+        time: 0,
+        favorites: false
+    }
+    if(i === items.length) items.push(item);
+    else if(i === 0) items.unshift(item);
+    else items.splice(i, 0, item);
     input.value = "";
     updateItems();
 }
@@ -78,6 +125,12 @@ window.model = {
 }
 
 window.onload = function () {
+    let data = localStorage.getItem('data');
+    if(data) {
+        window.model.data = JSON.parse(data);
+        $('addInput').value = window.model.data.content;
+        updateItems();
+    }
     $('addInput').onkeyup = function (event) {
         if(event.key === "Enter"){
             window.model.data.content = '';
@@ -87,6 +140,6 @@ window.onload = function () {
         else{
             window.model.data.content = $('addInput').value;
         }
+        flushStorage();
     };
-    $('itemCount').innerText = "0 items left";
 }
